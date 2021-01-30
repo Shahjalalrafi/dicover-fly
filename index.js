848239c2c6b703bb8d037a4bbd1f8cb56ad631d8
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-01-30 11:06:46 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,79 @@
+//increase button
+const increaseButtonFirstClass = document.getElementById('plus-btn1')
+const increaseBtnEconomy = document.getElementById('plus-btn2')
+//decrease button
+const decreaseBtnFirstClass = document.getElementById('minus-btn1')
+const decreaseBtnEconomy = document.getElementById('minus-btn2')
+
+//by default
+document.getElementById('sub-total').innerText  = ''
+document.getElementById('tex').innerText = ''
+document.getElementById('total').innerText = ''
+
+//ticket price
+const firstClassTicket = 150
+const economyTicket = 100
+
+//increase first class ticket
+increaseButtonFirstClass.addEventListener('click', () => {
+    handleTicket('first-class-input',true, firstClassTicket)
+})
+
+//increase economy ticket
+increaseBtnEconomy.addEventListener('click', () => {
+    handleTicket("economy-input",true,economyTicket)
+})
+
+//decrease first class ticket
+decreaseBtnFirstClass.addEventListener('click', () => {
+    handleTicket('first-class-input',false,firstClassTicket)
+})
+
+//decrease economy ticket
+decreaseBtnEconomy.addEventListener('click', () => {
+    handleTicket("economy-input",false, economyTicket)
+})
+
+//handle ticket function
+function handleTicket(inputId,isIncrease) {
+    const InputValue = document.getElementById(inputId).value
+    const inputNumber = parseFloat(InputValue)
+    let newInput = inputNumber
+    if(isIncrease == true) {
+        newInput = inputNumber +1
+    }else if(isIncrease == false && newInput > 0) {
+        newInput = inputNumber -1
+    }
+    document.getElementById(inputId).value = newInput
+
+    calculate()
+
+}
+
+//calcute function
+function calculate() {
+    const firstClassInputValue = document.getElementById('first-class-input').value
+    const econommyInputValue = document.getElementById('economy-input').value
+
+    const subtotal = (firstClassInputValue * firstClassTicket) + (econommyInputValue * economyTicket)
+    document.getElementById('sub-total').innerText = subtotal
+
+    const tex = subtotal * .1
+    document.getElementById('tex').innerText = tex
+
+    total = subtotal + tex
+    document.getElementById('total').innerText = total
+}
+
+const bookingBtn = document.getElementById('booking-btn')
+
+bookingBtn.addEventListener('click', () => {
+    const allSection = document.querySelector('.all-section')
+    allSection.style.display = 'none'
+
+    const footer = document.querySelector('footer')
+    footer.style.display = 'block'
+
+    const empty = document.getElementById('empty')
+    empty.append(`you buy ${document.getElementById('first-class-input').value} first-class ticket and ${document.getElementById('economy-input').value} economy ticket from us....`)
+})
